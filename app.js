fe282e07e63d5bd146cb919bdde49d918d156ca4
tron101 <northy8080@gmail.com>
2021-01-06 13:23:46 +0000

    Added new message to line 5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Using AWS and Git for DevOps example'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
